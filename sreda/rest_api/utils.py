c4d463dc54fe517ae59be3435f380c8746489b7d
Roman <graff049@gmail.com>
2022-02-08 00:22:24 +0300

    add docstrings and function for creating 2D arrays

--- sreda/rest_api/utils.py
+++ sreda/rest_api/utils.py
@@ -6,6 +6,7 @@ import numpy
 import matplotlib.pyplot as plt
 
 from .models import CustomUser
+from numpy import ndarray
 
 
 class Points:
@@ -15,36 +16,57 @@ class Points:
         self.standard_deviation = standard_deviation
         self.size = size
 
-    def generate_random_points(self):
+    def generate_random_points(self) -> tuple:
         """
         This function generate random points with Mean of X1 distribution, Mean of X2 distribution, Standard derivation
         and Resultant shape.
-
-        :return: points - resulted 2D array with x1, x2 values
+        :return: x1, x2 - resulted ndarray with x1, x2 values
         """
+
         x1 = numpy.random.normal(loc=self.mean_x1, scale=self.standard_deviation, size=self.size)
         x2 = numpy.random.normal(loc=self.mean_x2, scale=self.standard_deviation, size=self.size)
         return x1, x2
 
     @staticmethod
-    def extend_ndarrays(minus_one_x1, minus_one_x2, plus_one_x1, plus_one_x2):
-        minus_one_points = numpy.vstack((minus_one_x1, minus_one_x2)).T
-        plus_one_points = numpy.vstack((plus_one_x1, plus_one_x2)).T
-        result_array = numpy.append(minus_one_points, plus_one_points, 0)
+    def join_ndarrays(minus_one_2D, plus_one_2D) -> ndarray:
+        """
+        Join two 2D arrays.
+        :param minus_one_2D: 2D array of class-1 x1, x2 values
+        :param plus_one_2D: 2D array of class+1 x1, x2 values
+        :return: result_array - 2D array with all points
+        """
+
+        result_array = numpy.append(minus_one_2D, plus_one_2D, 0)
         return result_array
 
     @staticmethod
-    def create_points_csv(points):
+    def create_2d_arrays(minus_one_x1, minus_one_x2, plus_one_x1, plus_one_x2):
         """
-        This function create csv file with resulted points.
+        Create 2D arrays from 1D.
+        :param minus_one_x1: 1D array of class-1 x1 values
+        :param minus_one_x2: 1D array of class-1 x2 values
+        :param plus_one_x1: 1D array of class+1 x1 values
+        :param plus_one_x2: 1D array of class+1 x2 values
+        :return: minus_one_2D, plus_one_2D - resulted 2D arrays
+        """
 
-        :param points: resulted 2D array with x1, x2 values
-        :return: file_path - path to media for downloading the file
+        minus_one_2d = numpy.vstack((minus_one_x1, minus_one_x2)).T
+        plus_one_2d = numpy.vstack((plus_one_x1, plus_one_x2)).T
+        return minus_one_2d, plus_one_2d
+
+    @staticmethod
+    def create_points_csv(resulted_ndarray) -> tuple:
         """
+        Create csv file with resulted points.
+
+        :param resulted_ndarray: resulted 2D array with x1, x2 values
+        :return: file_path, filename - path to media for downloading the file and name of the file
+        """
+
         filename = str(uuid.uuid4())
         with open(f"./media/{filename}.csv", "w") as points_csv:
             writer = csv.writer(points_csv, delimiter=',')
-            writer.writerows(points)
+            writer.writerows(resulted_ndarray)
 
         file_path = points_csv.name.split('media/')[1]
 
@@ -52,12 +74,33 @@ class Points:
 
     @staticmethod
     def remove_files(csv_file_path, plot_file_path):
+        """
+        Create csv file with resulted points.
+
+        :param csv_file_path: path to csv file
+        :param plot_file_path: path to plot
+        """
+
         if os.path.exists(csv_file_path) and os.path.exists(plot_file_path):
             os.remove(csv_file_path)
             os.remove(plot_file_path)
+        else:
+            raise FileNotFoundError
 
     @staticmethod
-    def create_points_plot(minus_one_x1, minus_one_x2, plus_one_x1, plus_one_x2, filename):
+    def create_points_plot(minus_one_x1, minus_one_x2, plus_one_x1, plus_one_x2, filename) -> str:
+        """
+        Create a plot with resulted points.
+
+        :param minus_one_x1: 1D array of class-1 x1 values
+        :param minus_one_x2: 1D array of class-1 x2 values
+        :param plus_one_x1: 1D array of class+1 x1 values
+        :param plus_one_x2: 1D array of class+1 x2 values
+        :param filename - the name of the file
+
+        :return: plot_path, a path to the plot
+        """
+
         plt.scatter(minus_one_x1, minus_one_x2, c='red')
         plt.scatter(plus_one_x1, plus_one_x2, c='blue')
         plt.savefig(f"./media/{filename}.png")
@@ -71,7 +114,7 @@ class Points:
 
 def generate_points_create_csv():
     """
-    This function make all actions from generate points up to create csv file.
+    This function make all actions from generate points up to create csv file and plot by points.
 
     :return: csv_file_path - path to generated csv file
              plot_file_path - path to generated plot
@@ -84,13 +127,16 @@ def generate_points_create_csv():
     minus_one_x1, minus_one_x2 = class_minus_one_points.generate_random_points()
     plus_one_x1, plus_one_x2 = class_plus_one_points.generate_random_points()
 
-    # create one 2D array from 2 arrays
-    resulted_ndarray = Points.extend_ndarrays(minus_one_x1, minus_one_x2, plus_one_x1, plus_one_x2)
+    # create 2D arrays from 1D
+    minus_one_2d, plus_one_2d = Points.create_2d_arrays(minus_one_x1, minus_one_x2, plus_one_x1, plus_one_x2)
 
-    # create csv file
+    # create one 2D array with 100 points
+    resulted_ndarray = Points.join_ndarrays(minus_one_2d, plus_one_2d)
+
+    # create a csv file
     csv_file_path, filename = Points.create_points_csv(resulted_ndarray)
 
-    # create plot
+    # create a plot
     plot_file_path = Points.create_points_plot(minus_one_x1, minus_one_x2, plus_one_x1, plus_one_x2, filename)
 
     return csv_file_path, plot_file_path
